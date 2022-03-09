e8fba36cfe54784a49a87ca2b3a0cc4ee03f097c
Dudanii <mohitdudani7@gmail.com>
2022-03-09 20:14:48 +0530

    signup and login ui a bit done

--- login.php
+++ login.php
@@ -1,39 +1,35 @@
-<!DOCTYPE html>
-<html lang = "en">
+<html>
     <head>
-        <title> Ally</title>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="css/login-style.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Ally | Login</title>
+        <link href = "css/login7-style.css" rel = "stylesheet"  type= "text/css" >
     </head>
     <body>
         
-        <div class = "registerpage">
-            <header class= "header">
-                <a href = "index.php" class = "header-dummystocks"> ALLY </a>
-            </header>
-            <div class="wrapper">
-            <div class="register-form">
-                <div data-bn-type="text" class = "register-title">Log In</div>
-                <form action="includes/login.inc.php" method="post">
-                <div class="form">
-                    <div class = "form-group">
-                        <label class="label-email">Email</label>
-                        <input type="text" class="form-input" name="email"> 
-                    </div>
-                    <div class = "form-group-pwd">
-                        <label class="label-password">Password</label>
-                        <input type="password" class="form-input" name="password">
-                    </div>
-                    <br>
-                    <input type="checkbox" class="check-box"><span>Remember Me</span>
-                    
+        <div class="hero">
+            
+            <div class="form-box">
+                <div data-bn-type="text" class = "register-title">Login to Ally</div>
                 
-                    <a href="" class="createacc-box">
-                    <button class="btn-createaccount" data-bn-type="button" type ="sumbit" name="submit">Log In</button>
-                    </a> 
-                    </div>
-                </form>
+                <form action="includes/login.inc.php" method="post">
+                    <div class="form">
+                        
+                        <div class = "form-group-email">
+                            <label class="label-email">Email</label>
+                            <input type="text" class="form-input" name="email"> 
+                        </div>
+                        <div class = "form-group-pwd">
+                            <label class="label-password">Password</label>
+                            <input type="password" class="form-input" name="password">
+                        </div>
+                        <br>
+                        <input type="checkbox" class="check-box"><span class="checkbox-styling">Remember Me</span>
+                        
+                    
+                        <a href="" class="createacc-box">
+                        <button class="btn-createaccount" data-bn-type="button" type ="sumbit" name="submit">Log In</button>
+                        </a> 
+                        </div>
+                    </form>
                 <?php //to display the error message when filling out forms
 
                     if(isset($_GET["error"])){
--- signup.php
+++ signup.php
@@ -1,36 +1,35 @@
-<!DOCTYPE html>
-<html lang = "en">
+<html>
     <head>
-        <title> CipherX | Crypto for Beginners </title>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="css/signup-style.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Ally | Login</title>
+        <link href = "css/signup-style.css" rel = "stylesheet"  type= "text/css" >
     </head>
     <body>
-        <div class = "registerpage">
-            <header class= "header">
-                <a href = "index.php" class = "header-dummystocks"> CIPHERX </a>
-            </header>
-            <div class="wrapper">
-            <div class="register-form">
-                <div data-bn-type="text" class = "register-title">Create a free account</div>
-                <div data-bn-type="text" class = "register-subtitle">Welcome to CipherX</div>
-                <form action="includes/signup.inc.php" method="post">
-                <div class="form">
-                    <div class = "form-group-email">
-                        <label class="label-email">Email</label>
-                        <input type="text" name = "email" class="form-input" > 
-                    </div>
-                    <div class = "form-group-pwd">
-                        <label class="label-password">Password</label>
-                        <input type="password"  class="form-input" name="password">
-                    </div>
+        
+        <div class="hero">
+            
+            <div class="form-box">
+                <div data-bn-type="text" class = "register-title">Signup to Ally</div>
                 
-                    <a href="" class="createacc-box">
-                    <button class="btn-createaccount" type="submit" name="submit">Create Account</button>
-                    </a> 
-                    </div>
-                </form>
+                <form action="includes/login.inc.php" method="post">
+                    <div class="form">
+                        
+                        <div class = "form-group-email">
+                            <label class="label-email">Email</label>
+                            <input type="text" class="form-input" name="email"> 
+                        </div>
+                        <div class = "form-group-pwd">
+                            <label class="label-password">Password</label>
+                            <input type="password" class="form-input" name="password">
+                        </div>
+                        <br>
+                        <input type="checkbox" class="check-box"><span class="checkbox-styling">Remember Me</span>
+                        
+                    
+                        <a href="" class="createacc-box">
+                        <button class="btn-createaccount" data-bn-type="button" type ="sumbit" name="submit">Log In</button>
+                        </a> 
+                        </div>
+                    </form>
                 
                 <?php //to display the error message when filling out forms
 
