3aaebed2effca4782be97def1e5ce2b586d0c445
bhojwanichetana <97585653+bhojwanichetana@users.noreply.github.com>
2022-03-10 06:24:22 +0530

    landing page done

---
+++ about.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <meta name="copyright" content="MACode ID, https://macodeid.com">
+
+  <title>DigiGram - Free Agency Template</title>
+
+  <link rel="stylesheet" href="assets/vendor/animate/animate.css">
+
+  <link rel="stylesheet" href="assets/css/bootstrap.css">
+
+  <link rel="stylesheet" href="assets/css/maicons.css">
+
+  <link rel="stylesheet" href="assets/vendor/owl-carousel/css/owl.carousel.css">
+
+  <link rel="stylesheet" href="assets/css/theme.css">
+
+</head>
+<body>
+
+  <!-- Back to top button -->
+  <div class="back-to-top"></div>
+
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light shadow-sm">
+      <div class="container">
+        <a href="index.php" class="navbar-brand">ALLY<span class="text-primary"></span></a>
+
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="navbar-collapse collapse" id="navbarContent">
+          <ul class="navbar-nav ml-lg-4 pt-3 pt-lg-0">
+            <li class="nav-item">
+              <a href="index.php" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item active">
+              <a href="about.php" class="nav-link">About</a>
+            </li>
+            <li class="nav-item">
+              <a href="blog.php" class="nav-link">How to use</a>
+            </li>
+            <li class="nav-item">
+              <a href="contact.php" class="nav-link">Contact</a>
+            </li>
+          </ul>
+
+          <div class="ml-auto">
+            <a href="#" class="btn btn-outline rounded-pill">Login</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container mt-5">
+      <div class="page-banner">
+        <div class="row justify-content-center align-items-center h-100">
+          <div class="col-md-6">
+            <nav aria-label="Breadcrumb">
+              <ul class="breadcrumb justify-content-center py-0 bg-transparent">
+                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                <li class="breadcrumb-item active">About</li>
+              </ul>
+            </nav>
+            <h1 class="text-center">About Us</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <main>
+    <div class="page-section">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-6 py-3">
+            <div class="img-fluid text-center">
+              <img src="assets/img/bg_image_2.png" alt="">
+            </div>
+          </div>
+          <div class="col-lg-6 py-3 pr-lg-5">
+            <h2 class="title-section">We're <span class="marked">Pied Piper</span>, Team of FEs.</h2>
+            <div class="divider"></div>
+            <p>A team of 4 FEs passionate about programming.</p>
+            <a href="#" class="btn btn-primary">More Details</a>
+            <a href="#" class="btn btn-outline border ml-2">Success Stories</a>
+          </div>
+        </div>
+      </div> <!-- .container -->
+    </div> <!-- .page-section -->
+
+  </main>
+
+  <footer class="page-footer">
+    <div class="container">
+      
+
+      
+    </div> <!-- .container -->
+  </footer> <!-- .page-footer -->
+
+
+  <script src="assets/js/jquery-3.5.1.min.js"></script>
+
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+  <script src="assets/vendor/wow/wow.min.js"></script>
+
+  <script src="assets/vendor/owl-carousel/js/owl.carousel.min.js"></script>
+
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+
+  <script src="assets/vendor/animateNumber/jquery.animateNumber.min.js"></script>
+
+  <script src="assets/js/google-maps.js"></script>
+
+  <script src="assets/js/theme.js"></script>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,266 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <meta name="copyright" content="MACode ID, https://macodeid.com">
+
+  <title>ALLY | Home </title>
+
+  <link rel="stylesheet" href="assets/vendor/animate/animate.css">
+
+  <link rel="stylesheet" href="assets/css/bootstrap.css">
+
+  <link rel="stylesheet" href="assets/css/maicons.css">
+
+  <link rel="stylesheet" href="assets/vendor/owl-carousel/css/owl.carousel.css">
+
+  <link rel="stylesheet" href="assets/css/theme.css">
+
+</head>
+<body>
+
+  <!-- Back to top button -->
+  <div class="back-to-top"></div>
+
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light navbar-float">
+      <div class="container">
+        <a href="index.php" class="navbar-brand">ALLY<span class="text-primary"></span></a>
+
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="navbar-collapse collapse" id="navbarContent">
+          <ul class="navbar-nav ml-lg-4 pt-3 pt-lg-0">
+            <li class="nav-item active">
+              <a href="index.php" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item">
+              <a href="about.php" class="nav-link">About</a>
+            </li>
+            
+            <li class="nav-item">
+              <a href="blog.php" class="nav-link">How to Use</a>
+            </li>
+            <li class="nav-item">
+              <a href="contact.php" class="nav-link">Contact</a>
+            </li>
+          </ul>
+          <div class="ml-auto">
+            <a href="login.php" class="btn btn-outline rounded-pill">Login</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <div class="page-banner home-banner">
+      <div class="container h-100">
+        <div class="row align-items-center h-100">
+          <div class="col-lg-6 py-3 wow fadeInUp">
+            <h1 class="mb-4">It's time to care for the ones who cared for you.</h1>
+            <p class="text-lg mb-5">WIth ALLY, you can help your family member's feel loved.</p>
+
+            <a href="#" class="btn btn-outline border text-secondary">More Info</a>
+            <a href="#" class="btn btn-primary btn-split ml-2">How to Use <div class="fab"><span class="mai-play"></span></div></a>
+          </div>
+          <div class="col-lg-6 py-3 wow zoomIn">
+            <div class="img-place">
+              <img src="assets/img/1.jpg" alt="vector image of elderly"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <main>
+    <div class="page-section features">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-6 col-lg-4 py-3 wow fadeInUp">
+            <div class="d-flex flex-row">
+              <div class="img-fluid mr-3">
+                <img src="images/2.jpg" alt="vector image">
+              </div>
+              <div>
+                <h5>Photo memories with their loved ones.</h5>
+                <p>Photos of them along with their family and friends to remind them they are loved.</p>
+              </div>
+            </div>
+          </div>
+  
+          <div class="col-md-6 col-lg-4 py-3 wow fadeInUp">
+            <div class="d-flex flex-row">
+              <div class="img-fluid mr-3">
+                <img src="assets/img/icon_pattern.svg" alt="">
+              </div>
+              <div>
+                <h5>Reminders so they never forget.</h5>
+                <p>Easy to navigate and effective reminders to never again forget medicines or food.</p>
+              </div>
+            </div>
+          </div>
+  
+          <div class="col-md-6 col-lg-4 py-3 wow fadeInUp">
+            <div class="d-flex flex-row">
+              <div class="img-fluid mr-3">
+                <img src="assets/img/icon_pattern.svg" alt="">
+              </div>
+              <div>
+                <h5>Games and More</h5>
+                <p>Fun and engaging games and videos to make them feel good.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> <!-- .container -->
+    </div> <!-- .page-section -->
+  
+    <div class="page-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 py-3 wow zoomIn">
+            <div class="img-place text-center">
+              <img src="assets/img/bg_image_2.png" alt="">
+            </div>
+          </div>
+          <div class="col-lg-6 py-3 wow fadeInRight">
+            <h2 class="title-section">In India,<span class="marked">Dementia</span> affects more than 5.3 million people.</h2>
+            <div class="divider"></div>
+            <p>As age increases the chances of Dementia increase. A</p>
+            <div class="img-place mb-3">
+              <img src="assets/img/testi_image.png" alt="">
+            </div>
+            <a href="#" class="btn btn-primary">More Details</a>
+            <a href="#" class="btn btn-outline border ml-2">Success Stories</a>
+          </div>
+        </div>
+      </div> <!-- .container -->
+    </div> <!-- .page-section -->
+  
+    
+  
+    <div class="page-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 py-3 wow fadeInLeft">
+            <h2 class="title-section">We're <span class="marked">ready to</span> Serve you with best</h2>
+            <div class="divider"></div>
+            <p class="mb-5">We provide marketing services to startups & small business to looking for partner for their digital media, design & dev lead generation & communication.</p>
+            <a href="#" class="btn btn-primary">More Details</a>
+            <a href="#" class="btn btn-outline ml-2">See pricing</a>
+          </div>
+          <div class="col-lg-6 py-3 wow zoomIn">
+            <div class="img-place text-center">
+              <img src="assets/img/bg_image_3.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div> <!-- .container -->
+    </div> <!-- .page-section -->
+  
+    <div class="page-section">
+      <div class="container">
+        <div class="text-center wow fadeInUp">
+          <div class="subhead">Why Choose Us</div>
+          <h2 class="title-section">Your <span class="marked">Comfort</span> is Our Priority</h2>
+          <div class="divider mx-auto"></div>
+        </div>
+  
+        <div class="row mt-5 text-center">
+          <div class="col-lg-4 py-3 wow fadeInUp">
+            <div class="display-3"><span class="mai-shapes"></span></div>
+            <h5>High Performance</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, sit.</p>
+          </div>
+          <div class="col-lg-4 py-3 wow fadeInUp">
+            <div class="display-3"><span class="mai-shapes"></span></div>
+            <h5>Friendly Prices</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, sit.</p>
+          </div>
+          <div class="col-lg-4 py-3 wow fadeInUp">
+            <div class="display-3"><span class="mai-shapes"></span></div>
+            <h5>No time-confusing</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, sit.</p>
+          </div>
+        </div>
+      </div> <!-- .container -->
+    </div> <!-- .page-section -->
+
+    <div class="page-section">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-6 py-3 wow fadeInUp">
+            <h2 class="title-section">Get in Touch</h2>
+            <div class="divider"></div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br> Laborum ratione autem quidem veritatis!</p>
+  
+            <ul class="contact-list">
+              <li>
+                <div class="icon"><span class="mai-map"></span></div>
+                <div class="content">123 Fake Street, New York, USA</div>
+              </li>
+              <li>
+                <div class="icon"><span class="mai-mail"></span></div>
+                <div class="content"><a href="#">info@digigram.com</a></div>
+              </li>
+              <li>
+                <div class="icon"><span class="mai-phone-portrait"></span></div>
+                <div class="content"><a href="#">+00 1122 3344 55</a></div>
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-6 py-3 wow fadeInUp">
+            <div class="subhead">Contact Us</div>
+            <h2 class="title-section">Drop Us a Line</h2>
+            <div class="divider"></div>
+            
+            <form action="#">
+              <div class="py-2">
+                <input type="text" class="form-control" placeholder="Full name">
+              </div>
+              <div class="py-2">
+                <input type="text" class="form-control" placeholder="Email">
+              </div>
+              <div class="py-2">
+                <textarea rows="6" class="form-control" placeholder="Enter message"></textarea>
+              </div>
+              <button type="submit" class="btn btn-primary rounded-pill mt-4">Send Message</button>
+            </form>
+          </div>
+        </div>
+      </div> <!-- .container -->
+    </div> <!-- .page-section -->
+  
+  </main>
+
+  <footer class="page-footer">
+    
+  </footer> <!-- .page-footer -->
+
+
+  <script src="assets/js/jquery-3.5.1.min.js"></script>
+
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+  <script src="assets/vendor/wow/wow.min.js"></script>
+
+  <script src="assets/vendor/owl-carousel/js/owl.carousel.min.js"></script>
+
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+
+  <script src="assets/vendor/animateNumber/jquery.animateNumber.min.js"></script>
+
+  <script src="assets/js/google-maps.js"></script>
+
+  <script src="assets/js/theme.js"></script>
+
+
+</body>
+</html>
