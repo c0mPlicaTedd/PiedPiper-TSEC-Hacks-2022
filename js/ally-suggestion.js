52740f19cd3dbd22075be43114df49ae71081d29
c0mPlicaTedd <shirothegamer69@gmail.com>
2022-03-10 08:52:19 +0530

    suggestion

--- js/ally-suggestion.js
+++ js/ally-suggestion.js
@@ -1,7 +1,11 @@
-
+function createSuggestion(){
 var choices = ["Play games", "Go through memories", "Make your todo list","Video call your loved ones","Watch feel good videos"];
 const num = Math.floor(Math.random()*choices.length);
-const suggestion = choices[num];
-localStorage.setItem("suggestion",suggestion);
+localStorage.setItem("suggestion",choices[num]);
+const suggestion = localStorage.getItem("suggestion");
+console.log(suggestion);
 document.getElementById("suggestion").innerHTML = "Ally suggestion: "+suggestion;
+};
+
+window.onload = createSuggestion;
 
